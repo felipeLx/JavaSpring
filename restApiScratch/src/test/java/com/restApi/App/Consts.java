33ba299f32690ce5bae98d67f471a08df1ecd438
felipeLx <felipealvesfinanceiro@gmail.com>
2020-02-03 15:31:29 -0300

    react + springRest

---
+++ restApiScratch/src/test/java/com/restApi/App/Consts.java
@@ -0,0 +1,5 @@
+package com.restApi.App;
+
+public interface Consts {
+    int APPLICATION_PORT = 8080;
+}
---
+++ restApiScratch/src/test/java/com/restApi/App/SpringContextTest.java
@@ -0,0 +1,16 @@
+package com.restApi.App;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {RestApiApplication.class})
+class SpringContextTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
